908e58b0251b31e9ff59a9ea61e2c11b795abfad
Elusionz243 <strivelinkband@yahoo.com>
2022-01-04 07:03:39 +0600

    Initial Commit

---
+++ back-end/src/app.js
@@ -0,0 +1,21 @@
+cons path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
+const express = require("express");
+const cors = require("cors");
+const knex = require('./db/connection');
+
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
+const app = express();
+
+app.set('db', knex);
+app.use(cors());
+app.use(express.json());
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
